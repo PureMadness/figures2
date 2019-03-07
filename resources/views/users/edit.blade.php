13865e0d9963115ab92d3e43f115ce1c12e8254f
PureMadness <my.pure.mail@bk.ru>
2019-03-07 12:27:28 +0300

    Select replaced by input radio in user edit.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,35 +6,41 @@
         <form enctype="multipart/form-data" action="{{ route('user.save', $user->id)  }}" method="post">
             @csrf
             <div class="form-row mb-2">
-                <div class="col">
+                <div class="col-4">
                     <label for="login">Login</label>
-                    <input type="text" class="form-control form-control-lg @if($errors->get('login') || session('uniqueError')) is-invalid @endif"
+                    <input type="text"
+                           class="form-control form-control-lg @if($errors->get('login') || session('uniqueError')) is-invalid @endif"
                            name="login" id="login" placeholder="Login" value="{{ $user->login }}"/>
-                        @if ($errors->get('login'))
-                            @foreach ($errors->get('login') as $error)
-                                <div class="invalid-feedback">
-                                    {{ $error }}<br/>
-                                </div>
-                            @endforeach
-                        @endif
+                    @if ($errors->get('login'))
+                        @foreach ($errors->get('login') as $error)
+                            <div class="invalid-feedback">
+                                {{ $error }}<br/>
+                            </div>
+                        @endforeach
+                    @endif
                     @if (session('uniqueError'))
                         <div class="invalid-feedback">
                             {{ session('uniqueError') }}
                         </div>
                     @endif
                 </div>
-                <div class="col">
-                    <label for="role">Role</label>
-                    <select class="custom-select" name="role" id="role" size="2">
-                        <option value="admin" @if($user->role) selected @endif>Admin</option>
-                        <option value="user" @unless($user->role) selected @endunless>User</option>
-                    </select>
+                <div class="col-auto">
+                    <label for="role">Role:</label>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="role" name="role" value="admin" @if($user->role) checked @endif>Admin</input>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="role" name="role" value="user" @unless($user->role) checked @endunless>User</input>
+                    </div>
                 </div>
-                <div class="col">
-                    <label for="blocked">Blocked:</label>
-                    <select class="custom-select " name="blocked" id="blocked" size="2">
-                        <option value="yes" @if($user->blocked) selected @endif>Yes</option>
-                        <option value="no" @unless($user->blocked) selected @endunless>No</option>
+                <div class="col-auto">
+                    <label for="blocked">Blocked</label>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="blocked" name="blocked" value="yes" @if($user->blocked) checked @endif>Yes</input>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="blocked" name="blocked" value="no" @unless($user->blocked) checked @endunless>No</input>
+                    </div>
                     </select>
                 </div>
             </div>
